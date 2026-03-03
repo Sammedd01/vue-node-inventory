2d1ed7c13b83a785c90e3f4583d1427aec045e8b
sammed <157780733+Sammedd01@users.noreply.github.com>
2026-03-03 18:42:01 +0530

    combined frontend and backend

--- frontend/src/views/perchaseHistory.vue
+++ frontend/src/views/perchaseHistory.vue
@@ -31,7 +31,7 @@ import axios from "axios";
 const purchases = ref([]);
 
 onMounted(async () => {
-  const res = await axios.get("http://localhost:5000/api/purchases");
+  const res = await axios.get("/api/purchases");
   purchases.value = res.data;
 });
 
